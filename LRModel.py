cd85c98a8e1b708e164b1bc275574b849c9c9b95
Dvanegas334 <128095615+Dvanegas334@users.noreply.github.com>
2025-09-12 00:02:42 -0500

    Grafica de regresion lineal
    En esta actualización se agregó la visualización de la línea de regresión dentro del gráfico de dispersión, lo que permite observar de manera más clara la relación entre la distancia recorrida, el número de pasajeros y el precio. Además, se implementó la funcionalidad para que la gráfica se actualice automáticamente cada vez que se registran nuevos datos en tiempo real. Esto mejora la precisión del análisis y facilita la interpretación de los resultados directamente desde la aplicación.

--- LRModel.py
+++ LRModel.py
@@ -5,9 +5,9 @@ from sklearn.linear_model import LinearRegression
 app = Flask(__name__)
 
 data = {
-    "Distancia Recorrida": [2, 5, 7, 10, 3, 8, 12, 15, 6, 20, 18, 25, 30, 22, 28],
-    "Pasajeros": [1, 2, 3, 1, 4, 2, 5, 3, 2, 6, 4, 5, 7, 3, 6],
-    "Precio": [5500, 13000, 18000, 20500, 9500, 18500, 28500, 34500, 14500, 47000, 39500, 53500, 67500, 45500, 63500]
+    "Distancia Recorrida": [2],
+    "Pasajeros": [1],
+    "Precio": [5500]
 }
 
 df = pd.DataFrame(data)
