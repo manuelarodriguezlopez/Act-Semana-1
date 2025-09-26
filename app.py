bb16cdd0ec5389e8625e21ad55f22718e6aad85e
manuelarodriguezlopez <manuelarodriguez202218@gmail.com>
2025-09-26 13:18:34 -0500

    Adicionamiento de submenu
    Se hace revision a detalle de cada html para poder adicionar el menu de Tipos de algoritmos de clasificacion con el fin de que se pueda agregar el nuevo modelo y no se afecte la estructura de la pagina.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-import Naivebayes
+import NaiveBayes
 from flask import Flask, render_template, request
 import matplotlib
 matplotlib.use('Agg')
@@ -154,3 +154,4 @@ def naive_bayes():
                            image=resultados["image"])
 if __name__ == '__main__':
     app.run(debug=True)
+
